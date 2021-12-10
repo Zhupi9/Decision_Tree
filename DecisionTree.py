0a5766a1f10cf95ee61f286273878cd20fd2a9b4
Zhupi9 <mengxuanping@gmail.com>
2021-12-10 17:40:09 +0800

    raw version

--- DecisionTree.py
+++ DecisionTree.py
@@ -1,8 +1,109 @@
+fts = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
+       'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week']
+fts_domain = {'age': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
+              'workclass': ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov',
+                            'Without-pay', 'Never-worked'],
+              'fnlwgt': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
+              'education': ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc',
+                            '9th',
+                            '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'],
+              'education-num': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
+              'marital-status': ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed',
+                                 'Married-spouse-absent',
+                                 'Married-AF-spouse'],
+              'occupation': ['Tech-support', 'Craft-repair', 'Other-service', 'Sales', 'Exec-managerial',
+                             'Prof-specialty',
+                             'Handlers-cleaners', 'Machine-op-inspct', 'Adm-clerical', 'Farming-fishing',
+                             'Transport-moving',
+                             'Priv-house-serv', 'Protective-serv', 'Armed-Forces'],
+              'relationship': ['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'],
+              'race': ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'],
+              'sex': ['Female', 'Male'],
+              'capital-gain': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
+              'capital-loss': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
+              'hours-per-week': ['0.0', '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0']}
+labels = ['<=50K', '>50K']
 
-def algorithm(data):
-    tree=[]
+
+def major(label_value):
+    val = [0, 0]  # <=50K, >50K
+    for i in label_value:
+        if i == labels[0]:
+            val[0] += 1
+        else:
+            val[1] += 1
+    if val[0] > val[1]:
+        return 0
+    else:
+        return 1
+
+
+def info_gini(data, attr):
+    gini = 0
+    count = len(data[0])
+    print("count: ", count)
+    for val in fts_domain[attr]:
+        num_0 = 0
+        num_1 = 0
+        idx = fts.index(attr)
+        for i in range(count):
+            if data[idx][i] == val:
+                if data[13][i] == labels[0]:
+                    num_0 += 1
+                else:
+                    num_1 += 1
+        print(num_0, num_1)
+        total = num_0 + num_1
+        if total != 0:
+            gini += total / count * (1 - pow((num_0 / total), 2) - pow((num_1 / total), 2))
+
+    return gini
+
+
+def split(data, attributes):
+    min_gini = 0.5
+    best_attr = ""
+
+    for attr in attributes:
+        gini = info_gini(data, attr)
+        if gini < min_gini:
+            min_gini = gini
+            best_attr = attr
+    return best_attr
+
+
+def divide(data, bestAttributes, value):
+    examples = [[] for j in range(14)]
+    best_idx = fts.index(bestAttributes)
+    for index in range(len(data[0])):
+        if data[best_idx][index] == value:
+            for j in range(14):
+                examples[j].append(data[j][index])
+
+    return examples
+
+
+def make_tree(data, attributes):
+    p_label = major(data[13])
+    if len(data[0]) <= 1:
+        return labels[p_label]
+    elif data[13].count(data[13][0]) == len(data[13]):
+        return data[13][0]
+    else:
+        best_attr = split(data, attributes)
+        if best_attr == "":
+            return labels[p_label]
+        print("best attr:", best_attr)
+
+        tree = {best_attr: {}}
+        for val in fts_domain[best_attr]:
+            subset = divide(data, best_attr, val)
+            if len(subset[0]) == 0:
+                return labels[p_label]
+            else:
+                newAttr = [x for x in attributes]
+                newAttr.remove(best_attr)
+                subTree = make_tree(subset, newAttr)
+                tree[best_attr][val] = subTree
 
     return tree
-
-def gini():
-    return a
--- main.py
+++ main.py
@@ -3,21 +3,25 @@
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
-fts = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'capital-gain', 'capital-loss', 'hours-per-week']
-fts_domain = [['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
-            ['Private', 'Self-emp-not-inc', 'Self-emp-inc', 'Federal-gov', 'Local-gov', 'State-gov', 'Without-pay', 'Never-worked'],
-            ['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
-            ['Bachelors', 'Some-college', '11th', 'HS-grad', 'Prof-school', 'Assoc-acdm', 'Assoc-voc', '9th', '7th-8th', '12th', 'Masters', '1st-4th', '10th', 'Doctorate', '5th-6th', 'Preschool'],
-            ['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
-            ['Married-civ-spouse', 'Divorced', 'Never-married', 'Separated', 'Widowed', 'Married-spouse-absent', 'Married-AF-spouse'],
-            ['Tech-support', 'Craft-repair', 'Other-service', 'Sales', 'Exec-managerial', 'Prof-specialty', 'Handlers-cleaners', 'Machine-op-inspct', 'Adm-clerical', 'Farming-fishing', 'Transport-moving', 'Priv-house-serv', 'Protective-serv', 'Armed-Forces'],
-            ['Wife', 'Own-child', 'Husband', 'Not-in-family', 'Other-relative', 'Unmarried'],
-            ['White', 'Asian-Pac-Islander', 'Amer-Indian-Eskimo', 'Other', 'Black'],
-            ['Female', 'Male'],
-            ['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
-            ['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0'],
-            ['0.0', '1.0','2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0', '9.0']]
-            
+import DecisionTree
+from DecisionTree import *
+
+
+def zanding(data):
+    array = [[] for j in range(14)]
+    for dataline in data:
+        s = dataline.split(", ")
+        if len(s) == 15:
+            s.pop(13)
+            if "?" not in s:
+                for i in range(14):
+                    if i == 0 or i == 2 or i == 4 or i == 10 or i == 11 or i == 12:
+                        array[i].append(int(s[i]))
+                    else:
+                        array[i].append(s[i])
+    return array
+
+
 def preprocessing(train_data_path, test_data_path):
     contiousAttr = [0, 2, 4, 10, 11, 12]
     maxList = []
@@ -26,27 +30,22 @@ def preprocessing(train_data_path, test_data_path):
 
     with open(train_data_path, encoding="utf-8") as f:
         train_set = f.read().splitlines()
+    with open(test_data_path, encoding="utf-8") as f:
+        test_set = f.read().splitlines()
 
-    testArray = [[] for j in range(14)]
-    for dataline in train_set:
-        s = dataline.split(", ")
-        if len(s) == 15:
-            s.pop(13)
-            if "?" not in s:
-                for i in range(14):
-                    if i == 0 or i == 2 or i == 4 or i == 10 or i == 11 or i == 12:
-                        testArray[i].append(int(s[i]))
-                    else:
-                        testArray[i].append(s[i])
+    train_array = zanding(train_set)
+    test_array = zanding(test_set)
+
     for i in contiousAttr:
-        maxV = max(testArray[i]) + 1
-        minV = min(testArray[i])
+        maxV = max(train_array[i]) + 1
+        minV = min(train_array[i])
         maxList.append(maxV)
         minList.append(minV)
 
-    print(maxList)
-    print(minList)
-    return discretize_sample(testArray, contiousAttr,maxList,minList)
+    # print(maxList)
+    # print(minList)
+    return discretize_sample(train_array, contiousAttr, maxList, minList), discretize_sample(test_array, contiousAttr,
+                                                                                             maxList, minList)
 
 
 def discretize_sample(dataset, contiousAttr, maxList, minList):
@@ -66,14 +65,48 @@ def discretize_sample(dataset, contiousAttr, maxList, minList):
             sample[index] = str(new_value)
         discretizedata.append(sample)
     '''
-    print(dataset)
+    # print(dataset)
 
     return dataset
 
 
 def discretize_attribute(value, max, min):
-    gap = (max+1 - min) / 10  # discretize feature into ten categories
+    gap = (max + 1 - min) / 10  # discretize feature into ten categories
     return (int(value) - min) // gap
+
+
+def testing_single(record, tree):
+    true_label = record[13][0]
+    while isinstance(tree, dict):
+        treeKey = list(tree.keys())[0]
+        keyIndex = fts.index(treeKey)
+        value = record[keyIndex][0]
+        if value in tree[treeKey]:
+            if isinstance(tree[treeKey][value], dict):
+                tree = tree[treeKey][value]
+            else:
+                if tree[treeKey][value] == true_label:
+                    return 1
+                else:
+                    return 0
+        else:
+            if "<=50K" == true_label:
+                return 1
+            else:
+                return 0
+
+
+def testing(data, tree):
+    total = len(data[0])
+    correct = 0
+
+    for i in range(total):
+        array = [[] for j in range(14)]
+        for j in range(14):
+            array[j].append(data[j][i])
+        correct += testing_single(array, tree)
+
+    return correct / total
 
 
 # Press the green button in the gutter to run the script.
@@ -81,9 +114,10 @@ if __name__ == '__main__':
     train_data_file = "adult.data"
     test_data_file = "adult.test"
 
-    data = preprocessing(train_data_file, test_data_file)
+    train_data, test_data = preprocessing(train_data_file, test_data_file)
+    tree = make_tree(train_data, fts)
+    print(tree)
 
-    for sample in data:
-        print(sample)
+    print(testing(test_data, tree))
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
